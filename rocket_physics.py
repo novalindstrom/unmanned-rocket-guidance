8d62af75de1683b972edf816652fb974f6a7a0c9
Siggi-IT <signe.hjelmberg.4181@student.uu.se>
2026-09-17 14:48:30 +0200

    New functions

--- rocket_physics.py
+++ rocket_physics.py
@@ -11,32 +11,50 @@ R0 = [0,0] #startposition
 v0 = [0,0]
 c = 0.05 #kg/m
 Ang0 = (np.pi)/2 #startvinkel som ej får ändras innan 20m
+g = 9.18 # gravitation
 motortid = 10
 
 bransletank = 1 #behövs en sådan?
+
+tspan = [0, 10]
+
 # ------------ Helper - functions ------------
 # Kolla på de funktioner som ges i instruktionen??
-def angle(t):
-    return 0
+
 #retunerar massan beroende på förbränt bränsle
-def m(motortid): # motorn på 10 secunder förbränning, while eller bara en tillbaka?
-    if motortid == 0:
-        return 4
-    else:
-        motortid = motortid-1
-        return 8 - 0.4*(motortid + 1) #detta är bara vid t-steget
+def m(t): # motorn på 10 secunder förbränning, while eller bara en tillbaka?
+    if t in tspan:
+        return 8 - 0.4*(t), -0.4   #retunerar massan just nu vid detta tidssteg + derivatan
+
+    return 4 , 0 #retunerar massan när 10s har gått, då är förändringshastighet 0
 
 def uvec(t):
     u=np.zeros(2)
-    u[0] = k * np.cos(angle(t))
-    u[1] = k * np.sin(angle(t))
+    u[0] = k * np.cos(rocket_moves(y_pos))
+    u[1] = k * np.sin(rocket_moves(y_pos))
     return u
 
 
+# hastigheten = v
+# rocket_der = [x(t), y(t), Vx(t), Yx(t)]
 def F(t):
-    return m(t) * g - c(v(t)*v(t))
+    return m(t) * g - c * ((v(t))**2)
+
 
 def um(t): #hastighetsvektor
+    return
+
+
+#------------- Get steering angle -------------
+
+def rocket_moves(y_pos):
+    if y_pos < 20:
+        angle = np.pi / 2
+    else:
+        angle = np.pi / 4 # Change later
+
+    return angle
+
 
 
 # ------------ Numeric solver ----------------
@@ -58,23 +76,3 @@ def RK4_model(f, h, t, y0):
 
 # Anropas typ : 
 # t, y = RK4_model(ODE, steglängd, tspan, begynnelse villkoret)
-
-#---------Skal av idé till raketens riktning
-
-def rocket_moves(t, y):
-    yder = np.zeros(4)
-    x_position = y[0] # X positionen för var tidssteg
-    y_position = y[1] # Y postitionen dvs höjden för var tidssteg
-    x_hastighet = y[2] # derivatan av y är hastigheten
-    y_hastighet = y[3]
-
-    height = y_position # då det är y positionen i varje tidssteg i vår RK modell
-
-    if height < 20:
-        rocket_angle = np.pi / 2
-    else:
-        # rocket_angle = 
-                # här skulle vi kunna räkna ut vinkeln från nyvarande plats till målet med arctan maybe
-                # eller testa en konstant vinkel
-   
-   # sedan kolla hur mycket bränsle vi har 
