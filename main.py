b8a2d123c2cbe9d8a1a147aad9eae3beb3730667
Mawi2342 <matilde.wiberg.2342@student.uu.se>
2026-09-20 14:55:39 +0200

    last edits

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# This file will import the "physics-file" and 
-# run the actuall simulation -> plotting the graph
-import rocket_physics.py
+# This file will import the "physics-file" and run the actuall simulation -> plotting the graph
+import rocket_physics as rf
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
 
 # -------------- Numeric solver -----------------
 def RK4_model(f, h, t, y0):
@@ -26,7 +26,9 @@ h = 0.1
 t = np.arange(0, 10 + h, h)
 v0 = [0, 0, 0, 0]
 
-t, y = RK4_model(rocket_ODE, h, t, v0)
+t, y = RK4_model(rf.rocket_ODE, h, t, v0)
+
+sol = solve_ivp(rf.rocket_ODE, [0, 10], v0, t_eval=t, method="RK45")
 
 plt.scatter([80], [60], color='red', s=70, label="Mål (80, 60)")
 plt.title("Simulering av raketens bana", fontsize=14)
@@ -37,4 +39,5 @@ plt.ylim(0, 200)
 plt.legend()
 plt.grid(True)
 plt.plot(y[:, 0], y[:, 1], "-b", label="Raketens bana")
+plt.plot(sol.y[0], sol.y[1], "--g", linewidth=2, label="SciPy solve_ivp")
 plt.show()
